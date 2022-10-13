1f13cc08c051eaf3e8e2bf74c2071a36b03ba867
Shieda <drivnder123@gmail.com>
2022-10-13 03:23:09 +0300

    Second commit

--- js/app.js
+++ js/app.js
@@ -1,3 +1,31 @@
+// header and hero 
+
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    mobile_menu.classList.toggle('active');
+});
+
+document.addEventListener('scroll', () => {
+    var scroll_position = window.scrollY;
+    if (scroll_position > 250) {
+        header.style.backgroundColor = '#29323c';
+    } else {
+        header.style.backgroundColor = 'transparent';
+    }
+});
+
+menu_item.forEach((item) => {
+    item.addEventListener('click', () => {
+        hamburger.classList.toggle('active');
+        mobile_menu.classList.toggle('active');
+    });
+});
+
 // slider
 var swiper = new Swiper(".slide-content", {
   slidesPerView: 2,
@@ -52,6 +80,6 @@ window.onload = calcScrollValue;
 
 // loader
 
-$(window).on("load",function(){
+$(window).on("load", function () {
   $(".loader-wrapper").fadeOut("slow");
 });
